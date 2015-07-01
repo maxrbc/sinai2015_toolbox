56c10b738fc5ed7dc81c6b47f9b5630834b54d1f
Max R. Berrios Cruz <max.berrios@upr.edu>
2015-07-01 12:15:58 -0400

    FIX #1

--- testpage/settings.py
+++ testpage/settings.py
@@ -137,7 +137,7 @@ MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR , 'temporal_series' , 'media')
+    os.path.join(BASE_DIR , 'temporal_series' , 'media'),
     os.path.join(BASE_DIR , 'temporal_series' , 'static')
 )
 
