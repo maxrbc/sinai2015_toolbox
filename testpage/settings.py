855b8a106d228ad4168a4481fdd777db7bef9276
Max R. Berrios Cruz <max.berrios@upr.edu>
2015-07-01 15:56:58 -0400

    ADD

--- testpage/settings.py
+++ testpage/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -103,6 +102,7 @@ USE_TZ = True
 #
 #         #Heroku Config
 # ## ================================================
+# #import dj_database_url
 #
 # # Parse database configuration from $DATABASE_URL
 # DATABASES['default'] =  dj_database_url.config()
