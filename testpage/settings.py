d5e78d8ca061e707cda902d55873bc68f84c043c
Max R. Berrios Cruz <max.berrios@upr.edu>
2015-07-01 14:38:09 -0400

    FIX #4

--- testpage/settings.py
+++ testpage/settings.py
@@ -132,7 +132,7 @@ BASE_DIR = os.getcwd()
 STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+MEDIA_ROOT = os.path.join(STATIC_ROOT , 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
