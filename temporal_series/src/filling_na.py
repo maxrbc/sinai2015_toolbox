63ff0885ab0a6696b1c101585539952672805074
Max R. Berrios Cruz <max.berrios@upr.edu>
2015-07-12 01:14:23 -0400

    Filling NA corrected to be for group instead of single independent cell

--- temporal_series/src/filling_na.py
+++ temporal_series/src/filling_na.py
@@ -3,21 +3,21 @@ from numpy.random import randint , random_sample
 import numpy as np
 
 
-def making_random_na_test_column():
+def making_random_na_test_column(table):
     result  = map((lambda x,y : np.nan if x == 0 else y) ,
                     randint(2 , size = len(table.values)) ,
                     random_sample(len(table.values)))
     return result
 
-def fill_na_in_table(table , method = 'ffill'):
-    result = table.copy()
+def fill_na_in_table(table ,sample_id , method = 'ffill'):
+    result = table.copy().groupby(sample_id)
 
     if method == 'ffill' :
-        result  = result.ffill()
+        result = result.ffill()
         result = result.bfill()
 
     elif method == 'bfill' :
-        result = result.bfill()
+        result  = result.bfill()
         result  = result.ffill()
 
     elif method == 'interfill':
