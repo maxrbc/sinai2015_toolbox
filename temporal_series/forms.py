c0b62ff5f3e2a34a849ccc16c738b61b60a21a56
Max R. Berrios Cruz <max.berrios@upr.edu>
2015-08-17 20:29:11 -0400

    adding fixes to interfaces

--- temporal_series/forms.py
+++ temporal_series/forms.py
@@ -2,16 +2,17 @@ from django import forms
 
 
 class SubmitTable(forms.Form):
-	table_fd = forms.FileField(label='table_fp' , required=True)
-	table_timevar = forms.CharField(label='table_timevar' , required = True)
-	table_idVar = forms.CharField(label='table_idVar'  , required = True)
+    table_fd = forms.FileField(label='table_fp', required=True)
+    table_timevar = forms.CharField(label='table_timevar', required=True)
+    table_idVar = forms.CharField(label='table_idVar', required=True)
 
-	## For selection of the form parsing for the Dates
-	TIMEPOINT_CHOICES = ((1,'Continuous Form'),
-						 (0,'Discrete Form'))
+    ## For selection of the form parsing for the Dates
+    TIMEPOINT_CHOICES = ((1, 'Date Timepoints'),
+                         (0, 'Discrete Timepoints'))
 
-	COMPLITION_METHODS= (('bfill' , "Use Previous Field"),
-						 ('ffill' , "Use Next Field") ,
-						 ('inter' , "Use Linear Interpolation"))
- 	na_complition = forms.ChoiceField(widget = forms.Select , choices = COMPLITION_METHODS)
-	timepoint_type = forms.ChoiceField(widget = forms.Select , choices = TIMEPOINT_CHOICES)
+    COMPLITION_METHODS = (('bfill', "Use Previous Field"),
+                          ('ffill', "Use Next Field"),
+                          ('inter', "Use Linear Interpolation"))
+
+    na_complition = forms.ChoiceField(widget=forms.Select, choices=COMPLITION_METHODS)
+    timepoint_type = forms.ChoiceField(widget=forms.Select, choices=TIMEPOINT_CHOICES)
