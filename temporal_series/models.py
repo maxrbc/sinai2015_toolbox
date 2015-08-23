7c69b2eef9123d87b0617c555b6de43422ec6392
Max R. Berrios Cruz <max.berrios@upr.edu>
2015-08-23 02:02:10 -0400

    add function to take negative values as discrete timepoints

--- temporal_series/models.py
+++ temporal_series/models.py
@@ -13,52 +13,54 @@ from django.conf import settings
 ## ========================
 from .src.table_parser import Table_Parser as tp
 from .src.table_utils import convert_timepoint_to_date as convert
+from .src.table_utils import correct_timepoint_for_negatives as  correct
 from .src.filling_na import fill_na_in_table as fill_na
 
+
 class Graph(models.Model):
-	timevar = models.CharField(max_length = 100)
-	subjectID = models.CharField(max_length = 100)
+    timevar = models.CharField(max_length=100)
+    subjectID = models.CharField(max_length=100)
 
+    def get_graph(self, timevar_type, na_complition):
 
-	def get_graph(self , timevar_type , na_complition):
+        ## Table uploading and selection
+        import os
+        from django.conf import settings
 
-		## Table uploading and selection
-		import os
-		from django.conf import settings
+        table = tp(settings.MEDIA_ROOT + "/table_upload.txt")
 
-		table = tp(settings.MEDIA_ROOT+"/table_upload.txt")
+        if timevar_type == '0':
+            if min(table.get_table()[self.timevar]) < 0:
+                table.get_table()[self.timevar+"_corrected"] = correct(table.get_table()[self.timevar])
 
-		if timevar_type == '0':
+            timevar, new_table = convert(table.get_table().copy(), self.timevar+"_corrected")
+            table.set_table(new_table)
+            table.set_timevar(timevar)
+            table.set_IdVar(self.subjectID)
 
-			timevar , new_table = convert(table.get_table().copy(), self.timevar)
-			table.set_table(new_table)
-			table.set_timevar(timevar)
-			table.set_IdVar(self.subjectID)
+        else:
+            table.set_timevar(self.timevar)
+            table.set_IdVar(self.subjectID)
 
-		else:
-			table.set_timevar(self.timevar)
-			table.set_IdVar(self.subjectID)
+        ## Filling NA based on the argument send from
+        ## the web page form
+        table.set_table(fill_na(table.get_table(), self.subjectID, na_complition))
 
-		## Filling NA based on the argument send from
-		## the web page form
-		table.set_table(fill_na(table.get_table() ,self.subjectID, na_complition))
+        ## Table Google Vis data
+        description = table.generate_description()
+        data = table.generate_data()
 
-		## Table Google Vis data
-		description = table.generate_description()
-		data = table.generate_data()
+        ### Generation of the table as google formats
+        data_table = gviz_api.DataTable(description)
+        data_table.LoadData(data)
 
-		### Generation of the table as google formats
-		data_table = gviz_api.DataTable(description)
-		data_table.LoadData(data)
+        ## to take the correct order of the table
+        order_col = table.get_ordered_columns()
 
-		## to take the correct order of the table
-		order_col = table.get_ordered_columns()
+        ## Producing JScode to add on the html
+        code = data_table.ToJSCode("jscode_data", columns_order=order_col)
 
-		## Producing JScode to add on the html
-		code = data_table.ToJSCode("jscode_data" , columns_order=order_col)
-
-
-		templ = '''
+        templ = '''
 		<html>
   <script src="https://www.google.com/jsapi" type="text/javascript"></script>
   <script>
@@ -87,6 +89,6 @@ class Graph(models.Model):
   </body>
 </html>
 		'''
-		document = open(settings.BASE_DIR+"/temporal_series/templates/ts/graph.html" , "w")
-		document.write(templ % {'code' : code})
-		document.close()
+        document = open(settings.BASE_DIR + "/temporal_series/templates/ts/graph.html", "w")
+        document.write(templ % {'code': code})
+        document.close()
--- temporal_series/src/table_utils.py
+++ temporal_series/src/table_utils.py
@@ -1,51 +1,61 @@
-from datetime import date , timedelta
+from datetime import date, timedelta
 from django.conf import settings
+
 
 # Table loader only works for saving right now a txt
 # still need to be added version for the biom table
 
 def table_loader(FILE):
-	import os
-	with open(settings.MEDIA_ROOT+"/table_upload.txt" , "wb+") as destination:
-		for chunk in FILE.chunks():
-			destination.write(chunk)
+    import os
+    with open(settings.MEDIA_ROOT + "/table_upload.txt", "wb+") as destination:
+        for chunk in FILE.chunks():
+            destination.write(chunk)
+
 
 def generate_cluster_index(dates):
-	result = []
-	for i in dates:
-		if not i in result:
-			result.append(i)
-	return result
+    result = []
+    for i in dates:
+        if not i in result:
+            result.append(i)
+    return result
 
 
-	#timedelta parses the descrite value to a date
-	# and is added to the 1,1,1 date which default
-	# date for conversion
-def parse_timepoint_to_date(timepoints):
-	d = date(1,1,1)
-	result = []
-	for i in timepoints:
-		result.append(str(d + timedelta(i)))
-	return result
+# In case of negative values , then normalize by
+# lowest and correct identification by that value
+def correct_timepoint_for_negatives(timepoint):
+    # Do not care for order at this step , since
+    # we are just creating a reference index for
+    # the actual data.
+    temp = list(set(list(timepoint.copy())))
+    temp.sort()
+    result = []
+    for i in timepoint:
+        index = temp.index(i)
+        result.append(index)
+    return result
 
 
-	#makes a new column but with the
-	#timepoint variable parsed to a new
-	#variable with the date like value
-def convert_timepoint_to_date(table , timepoint):
-	parsed = parse_timepoint_to_date(table[timepoint])
-	print parsed
-	name = timepoint + "_to_date"
-	table[name] = parsed
-	return name,table
+# makes a new column but with the
+# timepoint variable parsed to a new
+# variable with the date like value
+def convert_timepoint_to_date(table, timepoint):
+    converted = []
+    d = date(1, 1, 1)
+    for i in table[timepoint]:
+        converted.append(str(d + timedelta(i)))
+
+    name = timepoint + "_to_date"
+    table[name] = converted
+    return name, table
+
 
 # this expects you passing a panda table
 # as the internal is desing for it
-def generate_discrete_timepoint(table , timestamp):
-	indexer = generate_cluster_index(table[timestamp])
-	result = []
-	for i in table[timestamp]:
-		result.append(indexer.index(i))
-	name = timestamp+"_descrete"
-	table[name] = result
-	return name,table
+def generate_discrete_timepoint(table, timestamp):
+    indexer = generate_cluster_index(table[timestamp])
+    result = []
+    for i in table[timestamp]:
+        result.append(indexer.index(i))
+    name = timestamp + "_descrete"
+    table[name] = result
+    return name, table
